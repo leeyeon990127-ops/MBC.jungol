feb7881c3d5bbd79180f0e14be59364c7eaab125
yeonsoo <leeyeon990127@gmail.com>
2026-06-30 18:54:29 +0900

    정올 9044:배열, for문

--- src/q1658/Main.java
+++ src/q1658/Main.java
@@ -1,10 +1,13 @@
-package p9018;
+package q1658;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        System.out.println(num);
 
 
     }
